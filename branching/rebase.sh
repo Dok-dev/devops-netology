acc76c39f1e071a00984f451c30ac971660d5086
Тимофей Бирюков <biryukov_tv@mail.ru>
2020-12-31 11:46:55 +0300

    new change to master

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,7 +2,7 @@
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
